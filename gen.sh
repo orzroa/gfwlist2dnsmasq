c720730db3d3b4432cdd537d8416e8d7c27c17f9
orzroa <orzroa@gmail.com>
2018-12-01 19:45:37 +0800

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -2,3 +2,5 @@
 ./gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5353 -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i
 git commit -am "auto updated conf file on `date`"
 git push
+
+cp dnsmasq_gfwlist_ipset.conf /var/www/html/
