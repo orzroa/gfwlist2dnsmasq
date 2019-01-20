8d5c5704640d5201a247dd1ebc05e3267b6521d0
orzroa <orzroa@gmail.com>
2019-01-20 15:26:23 +0800

    auto updated conf file on Sun Jan 20 15:26:23 CST 2019

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5353 -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i
+cat *.conf|md5sum|cut -d' ' -f1>conf.md5
 git commit -am "auto updated conf file on `date`"
 git push
 
