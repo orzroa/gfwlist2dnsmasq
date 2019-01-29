d0c0d20d8ab9d43358f6de3c86887a3550f195c6
orzroa <orzroa@gmail.com>
2019-01-29 13:30:05 +0800

    add git pull

--- gen.sh
+++ gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+git pull --rebase
+
 ./gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5353 -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i
 cat *.conf|md5sum|cut -d' ' -f1>conf.md5
 git commit -am "auto updated conf file on `date`"
 git push
 
-cp dnsmasq_gfwlist_ipset.conf /var/www/html/
