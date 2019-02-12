7f25a97ea48ead1cd0ab7ed7db40e5d8f6faf625
orzroa <orzroa@gmail.com>
2019-02-12 18:29:29 +0800

    auto updated conf file on Tue Feb 12 18:29:29 CST 2019

--- gen.sh
+++ gen.sh
@@ -3,8 +3,8 @@
 git pull --rebase
 
 ./gfwlist2dnsmasq.sh -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i --extra-domain-file speed.txt
-grep 'server=' dnsmasq_gfwlist_ipset.conf > gfwlist_dns.config
-grep 'ipset='  dnsmasq_gfwlist_ipset.conf > gfwlist_ipset.config
+grep '^server='       dnsmasq_gfwlist_ipset.conf > dnsmasq_gfwlist_server.conf
+sed -i '/^server=/d' dnsmasq_gfwlist_ipset.conf
 cat *.conf|md5sum|cut -d' ' -f1>conf.md5
 git commit -am "auto updated conf file on `date`"
 git push
