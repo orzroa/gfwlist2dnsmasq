d3c8512d37631747ab658891ea6baddfe93c23d5
orzroa <orzroa@gmail.com>
2019-02-12 15:16:42 +0800

    separate server= and ipset=

--- gen.sh
+++ gen.sh
@@ -2,7 +2,9 @@
 
 git pull --rebase
 
-./gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5353 -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i
+./gfwlist2dnsmasq.sh -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i --extra-domain-file speed.txt
+grep 'server=' dnsmasq_gfwlist_ipset.conf > gfwlist_dns.config
+grep 'ipset='  dnsmasq_gfwlist_ipset.conf > gfwlist_ipset.config
 cat *.conf|md5sum|cut -d' ' -f1>conf.md5
 git commit -am "auto updated conf file on `date`"
 git push
