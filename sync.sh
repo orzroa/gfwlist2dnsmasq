20af3d943e93fbbc3c4ad3385d8c17ad7636378a
orzroa <orzroa@gmail.com>
2019-02-23 19:49:02 +0800

    auto updated conf file on Sat Feb 23 19:49:02 CST 2019

--- sync.sh
+++ sync.sh
@@ -1,31 +1,17 @@
 #!/bin/sh
 
-tempdir="/etc/dnsmask.d"
-mkdir -p "$tempdir"
-cd "$tempdir"
+result=1
+cd /etc/dnsmasq.d
 
-oldMD5=`cat $tempdir/conf.md5`
-echo "oldMD5=$oldMD5"
-newMD5=`curl -s https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/conf.md5`
-echo "newMD5=$newMD5"
+mget https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_ipset.conf
+result=`expr $result \* $?`
+echo "result=$result"
 
-if [ -z "$newMD5" ]; then
-  echo "newMD5 is empty"
-  exit
-fi
+mget https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_server.conf
+result=`expr $result \* $?`
+echo "result=$result"
 
-if [ "$oldMD5" != "$newMD5" ]; then
-  curl -s -O https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_ipset.conf
-  curl -s -O https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_server.conf
-  dldMD5=`cat *.conf|md5sum|cut -d' ' -f1`
-  echo "dldMD5=$dldMD5"
-  if [ "$dldMD5" != "$newMD5" ]; then
-    echo 'Downloaded file md5 check error!'
-  else
-    mv -f *.conf /etc/dnsmasq.d/
-    echo "$dldMD5" >$tempdir/conf.md5
-    /etc/init.d/dnsmasq restart
-    echo 'File updated!'
-  fi
+if [ $result -eq 0 ]; then
+  /etc/init.d/dnsmasq restart
 fi
 
