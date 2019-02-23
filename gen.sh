1cc27195edff00e0f0086d5a0bee0151e96ee25e
orzroa <orzroa@gmail.com>
2019-02-23 19:52:25 +0800

    remove conf.md5

--- gen.sh
+++ gen.sh
@@ -5,7 +5,6 @@ git pull --rebase
 ./gfwlist2dnsmasq.sh -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i --extra-domain-file speed.txt
 grep '^server='       dnsmasq_gfwlist_ipset.conf > dnsmasq_gfwlist_server.conf
 sed -i '/^server=/d' dnsmasq_gfwlist_ipset.conf
-cat *.conf|md5sum|cut -d' ' -f1>conf.md5
 
 for emm in `ls *.conf`
 do
