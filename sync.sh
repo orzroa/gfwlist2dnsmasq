671c49a97b9b6bb00f0a27bb37d727a186c92f24
orzroa <orzroa@gmail.com>
2019-02-23 19:50:50 +0800

    use mget to sync

--- sync.sh
+++ sync.sh
@@ -3,13 +3,12 @@
 result=1
 cd /etc/dnsmasq.d
 
-mget https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_ipset.conf
-result=`expr $result \* $?`
-echo "result=$result"
-
-mget https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/dnsmasq_gfwlist_server.conf
-result=`expr $result \* $?`
-echo "result=$result"
+for emm in  `ls *.conf`
+do
+  mget https://raw.githubusercontent.com/orzroa/gfwlist2dnsmasq/master/$emm
+  result=`expr $result \* $?`
+  echo "result=$result"
+done
 
 if [ $result -eq 0 ]; then
   /etc/init.d/dnsmasq restart
