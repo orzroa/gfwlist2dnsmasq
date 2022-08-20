72bd9eacc5a7852d0b02e834374ef2e68d0824d9
leo <leo@lee.family>
2022-08-20 20:37:23 +0800

    bugfix: when gfwlist2dnsmasq.sh failed we will get an empty dnsmasq_gfwlist_server.conf

--- gen.sh
+++ gen.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+echo "========================================================================================="
+dater
+
 git pull --rebase
 
 ./gfwlist2dnsmasq.sh -s gfwlist -o dnsmasq_gfwlist_ipset.conf -i --extra-domain-file speed.txt
+
+if [ $? -ne 0 ]; then
+  echo "./gfwlist2dnsmasq.sh failed"
+  exit
+fi
+
+if [ `grep '^server=' dnsmasq_gfwlist_ipset.conf|wc -l` -eq 0 ]; then
+  echo "dnsmasq_gfwlist_server.conf is empty"
+  exit
+fi
+
 grep '^server='       dnsmasq_gfwlist_ipset.conf > dnsmasq_gfwlist_server.conf
 sed -i '/^server=/d' dnsmasq_gfwlist_ipset.conf
 
